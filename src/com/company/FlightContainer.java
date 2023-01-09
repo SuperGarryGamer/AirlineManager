da190537034ad1bd759702fffc02b15299ebbbf2
supergarrygamer <supergarrygamer@gmail.com>
2023-01-09 15:25:38 +0100

    add fun stuuf

--- src/com/company/FlightContainer.java
+++ src/com/company/FlightContainer.java
@@ -4,8 +4,8 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
 public class FlightContainer {
-    GridPane gp;
-    Flight flight;
+    public GridPane gp;
+    public Flight flight;
 
     public FlightContainer(Flight flight) {
         this.flight = flight;
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,15 +2,19 @@ package com.company;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main extends Application {
 
     List<Flight> flights = new ArrayList<Flight>();
+
+    Button addFlightViewButton = new Button("Add flight");
+    ScrollPane flightsScrollPane = new ScrollPane();
 
     public static void main(String[] args) {
         launch(args);
@@ -22,6 +26,12 @@ public class Main extends Application {
         GridPane viewFlight = new GridPane();
         Scene addFlightView = new Scene(addFlight);
         Scene viewFlightView = new Scene(viewFlight);
+        System.out.println("starting app =w=");
 
+
+        viewFlight.add(addFlightViewButton, 0, 0);
+
+        primaryStage.setScene(viewFlightView);
+        primaryStage.show();
     }
 }
