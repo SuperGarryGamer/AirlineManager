f73407b0d88ce12d4448cefc2ea8c8640611e5d0
supergarrygamer <supergarrygamer@gmail.com>
2023-01-10 13:27:20 +0100

    add fun stuuf2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,13 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,6 +20,18 @@ public class Main extends Application {
 
     Button addFlightViewButton = new Button("Add flight");
     ScrollPane flightsScrollPane = new ScrollPane();
+    StackPane flightsStackPane = new StackPane();
+    Image mapImage;
+
+    {
+        try {
+            mapImage = new Image(getClass().getResource("worldmap.png").toURI().toString());
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    ImageView worldMap = new ImageView(mapImage);
 
     public static void main(String[] args) {
         launch(args);
@@ -24,12 +41,14 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         GridPane addFlight = new GridPane();
         GridPane viewFlight = new GridPane();
-        Scene addFlightView = new Scene(addFlight);
-        Scene viewFlightView = new Scene(viewFlight);
+        Scene addFlightView = new Scene(addFlight, 1280, 720);
+        Scene viewFlightView = new Scene(viewFlight, 1280, 720);
         System.out.println("starting app =w=");
 
-
         viewFlight.add(addFlightViewButton, 0, 0);
+        viewFlight.add(flightsScrollPane, 0, 1);
+        flightsScrollPane.setContent(flightsStackPane);
+        viewFlight.add(worldMap, 1, 0, 1, 2);
 
         primaryStage.setScene(viewFlightView);
         primaryStage.show();
