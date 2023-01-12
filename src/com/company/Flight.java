39e7567e62cb691975c5141a7dbc2bce7635eff0
aupradies <gdwotek@gmail.com>
2023-01-12 16:15:32 +0100

    se

--- src/com/company/Flight.java
+++ src/com/company/Flight.java
@@ -3,8 +3,55 @@ package com.company;
 import java.time.LocalDate;
 
 public class Flight {
-    public String arrivalAirport, departureAirport;
-    public LocalDate arrivalTime, departureTime;
+    private String arrivalAirport, departureAirport;
+    private LocalDate arrivalTime, departureTime;
 
+    public Flight(String arrivalAirport, String departureAirport, LocalDate arrivalTime, LocalDate departureTime) {
+        this.arrivalAirport = arrivalAirport;
+        this.departureAirport = departureAirport;
+        this.arrivalTime = arrivalTime;
+        this.departureTime = departureTime;
+    }
 
+    public String getArrivalAirport() {
+        return arrivalAirport;
+    }
+
+    public void setArrivalAirport(String arrivalAirport) {
+        this.arrivalAirport = arrivalAirport;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public void setDepartureAirport(String departureAirport) {
+        this.departureAirport = departureAirport;
+    }
+
+    public LocalDate getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(LocalDate arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public LocalDate getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(LocalDate departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Flight{" +
+                "arrivalAirport='" + arrivalAirport + '\'' +
+                ", departureAirport='" + departureAirport + '\'' +
+                ", arrivalTime=" + arrivalTime +
+                ", departureTime=" + departureTime +
+                '}';
+    }
 }
